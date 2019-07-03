9702766856cd2a1641b43bce531e2dbf864a6ecf
unknown <vaibhav.kaul2@gnmail.com>
2019-07-04 00:43:38 +0530

    slider with text

--- js/navbar.js
+++
@@ -1,6 +0,0 @@
-const hamburger=document.querySelector('.hamburger');
-const navLinks=document.querySelector('.nav-links');
-const link=document.querySelectorAll('nav-links li');
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-});
